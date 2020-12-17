7fe3ea95f43578a90b83137dadaa51f9475b53c7
Idanturjeman <idan_turjeman29@walla.co.il>
2020-12-17 15:43:05 +0200

    changes in ex2 class

--- src/GameData/Ex2.java
+++ src/GameData/Ex2.java
@@ -22,12 +22,12 @@ import java.util.List;
  */
 public class Ex2 implements Runnable{
 
-    private static FrameData _win;                                     //game window
-    private static Arena _ar;                                      //game arena
-    private static long _id = -1;                                  //login id
-    private static int _scenario = 0;                              //game level [0,23]
-    private static ArrayList<Thread> _agents = new ArrayList<>();  //list of agent threads
-    private static directed_weighted_graph _graph;                 //the graph shown
+    private static FrameData _win;
+    private static Arena _ar;
+    private static long _id = -1;
+    private static int _scenario = 0;
+    private static ArrayList<Thread> _agents = new ArrayList<>();
+    private static directed_weighted_graph _graph;
 
     /** main method, launches the game and the GUIs.
      * @param a a[0] = id, a[1] = level.
@@ -35,10 +35,10 @@ public class Ex2 implements Runnable{
      */
     public static void main(String[] a) {
         Thread client = new Thread(new Ex2());
-        if(a.length == 0){                         //check if there are given arguments
+        if(a.length == 0){
             FrameData log = new FrameData();
-            log.frameData();                      //launches the login GUI
-            while(log.isOpen()){                   //wait for the user to choose his options
+            log.frameData();
+            while(log.isOpen()){
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
@@ -47,11 +47,11 @@ public class Ex2 implements Runnable{
             }
             _id = log.getID();
             _scenario = log.getScenario();
-            log.dispose();                        //set the id and level and closes the login GUI
+            log.dispose();
         }
         else{
-            _id = Long.parseLong(a[0]);          //in case of command arguments just set them
-            _scenario = Integer.parseInt(a[1]);  //without a UI
+            _id = Long.parseLong(a[0]);
+            _scenario = Integer.parseInt(a[1]);
         }
         client.start();
     }
@@ -61,7 +61,7 @@ public class Ex2 implements Runnable{
      */
     @Override
     public void run() {
-        game_service game = Game_Server_Ex2.getServer(_scenario);  //set level and login if there is id
+        game_service game = Game_Server_Ex2.getServer(_scenario);
         if(_id > -1){
             long id = _id;
             game.login(id);
@@ -71,14 +71,14 @@ public class Ex2 implements Runnable{
         game.startGame();
         _win.setTitle("Ex2 - OOP: Gotta catch them all! "+game.toString());
         int ind=0;
-        long dt=1000/60;  //frame rates 60FPS
-        moveAgents(game); //after the game was set, initialize the agents and start them
+        long dt=1000/60;
+        moveAgents(game);
         for(Thread thread : _agents){
             thread.start();
         }
         while(game.isRunning()) {
             try {
-                if(ind%1==0) {_win.repaint();}  //repaint the window in the wanted rate
+                if(ind%1==0) {_win.repaint();}
                 Thread.sleep(dt);
                 ind++;
             }
@@ -86,21 +86,21 @@ public class Ex2 implements Runnable{
                 e.printStackTrace();
             }
         }
-        String res = game.toString();    //print result and exit
+        String res = game.toString();
         System.out.println(res);
         System.exit(0);
     }
 
-    // first move, sets the game arena and agents.
+
     private void moveAgents(game_service game) {
-        String lg = game.move();                             //get game data from the server
-        List<CL_Agent> log = Arena.getAgents(lg, _graph);    //set agents
+        String lg = game.move();
+        List<CL_Agent> log = Arena.getAgents(lg, _graph);
         _ar.setAgents(log);
-        String fs =  game.getPokemons();                     //set pokemons
+        String fs =  game.getPokemons();
         List<CL_Pokemon> ffs = Arena.json2Pokemons(fs);
         _ar.setPokemons(ffs);
-        MoveData mover = new MoveData(_ar,_graph,game,log.size()); //the object responsible to move the agents
-        for(int i=0;i<log.size();i++) {                      //initialize the agents' threads
+        MoveData mover = new MoveData(_ar,_graph,game,log.size());
+        for(int i=0;i<log.size();i++) {
             CL_Agent ag = log.get(i);
             Agent agent = new Agent(ag,game,mover);
             Thread thread = new Thread(agent);
@@ -108,20 +108,19 @@ public class Ex2 implements Runnable{
         }
     }
 
-    //initialize the game's elements and GUI.
     private void init(game_service game) {
         String g = game.getGraph();
         String fs = game.getPokemons();
         DWGraphs_Algo ga = new DWGraphs_Algo();
-        _graph = ga.Json2Graph(g);               //set game graph
-        _ar = new Arena();                       //set game arena
+        _graph = ga.Json2Graph(g);
+        _ar = new Arena();
         _ar.setGraph(_graph);
         _ar.setPokemons(Arena.json2Pokemons(fs));
-        _win = new FrameData("Ex2", game, _scenario);        //set game GUI
+        _win = new FrameData("Ex2", game, _scenario);
         _win.setSize(1000, 700);
         _win.update(_ar);
         _win.show();
-        String info = game.toString();   //build the game from the server
+        String info = game.toString();
         JSONObject line;
         try {
             line = new JSONObject(info);
@@ -131,10 +130,10 @@ public class Ex2 implements Runnable{
             System.out.println(game.getPokemons());
             ArrayList<CL_Pokemon> cl_fs = Arena.json2Pokemons(game.getPokemons());
             for(int a = 0;a<cl_fs.size();a++) { Arena.updateEdge(cl_fs.get(a),_graph);}
-            for(int a = 0;a<rs;a++) {    //default setting of the agents on the graph
+            for(int a = 0;a<rs;a++) {
                 int ind = a%cl_fs.size();
                 CL_Pokemon c = cl_fs.get(ind);
-                int nn = c.get_edge().getSrc(); //mainly close to pokemons and far apart
+                int nn = c.get_edge().getSrc();
                 game.addAgent(nn);
             }
         }
